9272cfbfa354a9991cf98cd367b6b87469436f15
codeots-tds <thedatascape@gmail.com>
2024-10-01 18:41:35 -0400

    adding files for datawarehousing project

---
+++ 3.data_warehouse_project/README.md
@@ -0,0 +1,118 @@
+# Udacity Datawarehouse Project
+
+## Table of Contents:
+- [Overview](#overview)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+---
+
+## Overview:
+This project is part of Udacity's Data Engineering Nanodegree program and focuses on building an AWS Redshift-based data warehouse. The goal is to design a cloud-based data warehouse solution to handle large-scale data from an imaginary music streaming company, Sparkify.
+
+The project involves setting up ETL (Extract, Transform, Load) pipelines that move data from Amazon S3 to Amazon Redshift, transforming it into a star-schema format optimized for analytical queries.
+
+## Features:
+List the key features of your project:
+- Design a data warehouse using AWS Redshift to handle large volumes of data.
+- Create an ETL pipeline that extracts data from S3, stages it in Redshift, and loads it into fact and dimension tables for analytics
+- Optimize the database schema for performance using a star schema structure.
+
+## Installation:
+Step-by-step instructions to install and run the project locally:
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/your-username/your-project.git
+
+
+## Datasets:
+There are two main datasets for this project which should be stored in Amazon S3 bucket.
+- Song Data: Metadata about songs and the artists involved.
+    - Example: s3://udacity-dend/song_data
+
+- Log Data: Logs of user activity on the Sparkify app based on a simulation of events.
+    - Example: s3://udacity-dend/log_data
+
+## Project Structure:
+The project contains the files below:
+- create_tables.py: Script to create staging, fact, and dimension tables in Redshift.
+- etl.py: Script to extract data from S3, stage it in Redshift, and load it into the final tables.
+- sql_queries.py: Contains all the SQL queries used in the project for table creation and data insertion.
+- dwh.cfg: Configuration file that contains AWS credentials and other Redshift connection details.
+
+## Database Schema:
+
+This data warehouse is designed using a star schema with one fact table and 4 dimensional tables.
+Fact Tables:
+
+- songplays table:
+songplay_id INT PRIMARY KEY - primary key(auto increment)
+start_time TIMESTAMP - timestamp of the song playing
+user_id INT - id of user
+level VARCHAR - subscription level
+song_id INT - song id number
+artist_id INT - artist id number
+session_id INT - session id number
+location VARCHAR - location of user
+user_agent VARCHAR - user agent info
+
+Dimensional Tables:
+
+- users table:
+user_id INT PRIMARY KEY -user id number
+first_name VARCHAR -user first name
+last_name VARCHAR -user last name
+gender CHAR(1) -user gender
+level VARCHAR -subscription level
+
+- songs table:
+song_id VARCHAR PRIMARY KEY, - unqiue song identifier
+title VARCHAR NOT NULL - song title
+artist_id INT NOT NULL - artist id number
+year INT - year the song was released
+duration FLOAT - duration of the song playtime in seconds
+
+- artist table:
+artist_id INT PRIMARY KEY - unique artist id
+name VARCHAR NOT NULL - name of artist
+location VARCHAR - artist's home town
+latitude FLOAT - latitude of artists location
+longitude FLOAT - longitude of the artist's location
+
+- time table:
+start_time TIMESTAMP PRIMARY KEY - timestamp of start of the song
+hour INT NOT NULL - hour from timestamp
+day INT NOT NULL - day from timestamp
+week INT NOT NULL -week from timestamp
+month INT NOT NULL -month from timestamp
+year INT NOT NULL - year from timestamp
+weekday INT NOT NULL - day from timestamp
+
+## ETL Pipeline:
+Purpose:
+- The goal of this ETL pipeline is to extract json data from S3 bucket, load it into the staging
+tables in Redshift, transform and load the data into the corresponding fact and dimension tables.
+
+Steps:
+1. Staging: Copy the raw data into the S3 bucket into staging tables in Redshift.
+2. Transformation: Taking the requirement business logic and transforming the data appropriately.
+3. Loading the transformed data into the corresponding fact and dimension tables.
+
+
+## How to Run:
+1. AWS IAM Role: Have the proper AWS IAM Role configured with access to S3 and Redshift.
+2. AWS Redshift Cluster: Create a Redshift cluster using AWS and record the connection 
+details(host, username, database, password).
+
+## Steps:
+1. Configure dwh.cfg with your redshift and aws credentials.
+2. Create your database tables with your schema by running create_tables.py
+3. Run the ETL pipeline to load the data into the corresponding tables.
+
+## Expected Result:
+After the ETL pipeline is executed, the data from the song and log datasets will be stored in the 
+tables in the star schema format in your Redshift cluster. The data should then be ready for querying.
---
+++ 3.data_warehouse_project/sql_queries.py
@@ -0,0 +1,217 @@
+import configparser
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+staging_events_table_create = ("""
+    CREATE TABLE IF NOT EXISTS staging_events (
+    event_id      INT,
+    artist        VARCHAR,
+    auth          VARCHAR,
+    first_name    VARCHAR,
+    gender        CHAR(1),
+    item_in_session INT,
+    last_name     VARCHAR,
+    length        FLOAT,
+    level         VARCHAR,
+    location      VARCHAR,
+    method        VARCHAR,
+    page          VARCHAR,
+    registration  BIGINT, 
+    session_id    INT, 
+    song          VARCHAR,
+    status        INT, 
+    ts            BIGINT,
+    user_agent    VARCHAR,
+    user_id       INT
+);
+""")
+
+staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs (
+    num_songs        INT,
+    artist_id        VARCHAR,
+    artist_latitude  FLOAT,
+    artist_longitude FLOAT,
+    artist_location  VARCHAR,
+    artist_name      VARCHAR,
+    song_id          VARCHAR,
+    title            VARCHAR,
+    duration         FLOAT,
+    year             INT 
+);
+""")
+
+# Fact Table
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id INT IDENTITY(0,1) NOT NULL PRIMARY KEY,
+    start_time TIMESTAMP NOT NULL,
+    user_id INT NOT NULL,
+    level VARCHAR NOT NULL,
+    song_id VARCHAR NOT NULL,
+    artist_id VARCHAR NOT NULL,
+    session_id INT NOT NULL,
+    location VARCHAR NOT NULL,
+    user_agent VARCHAR NOT NULL
+);
+""")
+
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    gender CHAR(1),
+    level VARCHAR
+);
+""")
+
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id VARCHAR PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    artist_id VARCHAR NOT NULL,
+    year INT,
+    duration FLOAT
+);
+""")
+
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    location VARCHAR,
+    latitude FLOAT,
+    longitude FLOAT
+);
+""")
+
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time TIMESTAMP PRIMARY KEY,
+    hour INT NOT NULL,
+    day INT NOT NULL,
+    week INT NOT NULL,
+    month INT NOT NULL,
+    year INT NOT NULL,
+    weekday INT NOT NULL
+);
+""")
+
+# STAGING TABLES
+"""
+-Pulling log data from S3 bucket into staging_events table.
+-From log_data directory in your S3 bucket
+-select the IAM role that allows your Redshift cluster permission to access S3 bucket
+-JSON path file tells Redshift how to parse your raw JSON files and map to columns in staging_events table.
+-Region is 'us-west-2' where the bucket is located.
+"""
+staging_events_copy = ("""
+COPY staging_events
+FROM 's3://your-bucket/log_data'
+IAM_ROLE 'arn:aws:iam::your-iam-role-id:role/RedshiftCopyRole'
+JSON 's3://your-bucket/log_json_path.json'
+REGION 'us-west-2';
+""")
+
+"""
+-Pulling song metadata from S3 into staging_songs table.
+-Since song data is structured well, you can use 'auto' keyword so Redshift auto-maps the fields to
+columns in staging_songs table
+"""
+staging_songs_copy = ("""
+COPY staging_songs
+FROM 's3://your-bucket/song_data'
+IAM_ROLE 'arn:aws:iam::your-iam-role-id:role/RedshiftCopyRole'
+JSON 'auto'
+REGION 'us-west-2';
+""")
+
+# FINAL TABLES
+
+# Insert Data
+songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+SELECT
+    TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second' AS start_time,
+    se.user_id,
+    se.level,
+    ss.song_id,
+    ss.artist_id,
+    se.session_id,
+    se.location,
+    se.user_agent
+FROM staging_events se
+JOIN staging_songs ss
+    ON se.song = ss.title
+    AND se.artist = ss.artist_name
+WHERE se.page = 'NextSong';
+""")
+
+user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+SELECT DISTINCT
+    se.user_id,
+    se.first_name,
+    se.last_name,
+    se.gender,
+    se.level
+FROM staging_events se
+WHERE se.user_id IS NOT NULL;
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+SELECT DISTINCT
+    ss.song_id,
+    ss.title,
+    ss.artist_id,
+    ss.year,
+    ss.duration
+FROM staging_songs ss
+WHERE ss.song_id IS NOT NULL;
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+SELECT DISTINCT
+    ss.artist_id,
+    ss.artist_name AS name,
+    ss.artist_location AS location,
+    ss.artist_latitude AS latitude,
+    ss.artist_longitude AS longitude
+FROM staging_songs ss
+WHERE ss.artist_id IS NOT NULL;
+""")
+
+time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+SELECT DISTINCT
+    TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second' AS start_time,
+    EXTRACT(hour FROM TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second'),
+    EXTRACT(day FROM TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second'),
+    EXTRACT(week FROM TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second'),
+    EXTRACT(month FROM TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second'),
+    EXTRACT(year FROM TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second'),
+    EXTRACT(weekday FROM TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second')
+FROM staging_events se
+WHERE se.ts IS NOT NULL;
+""")
+
+# QUERY LISTS
+create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
