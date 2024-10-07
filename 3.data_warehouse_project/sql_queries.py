f4fc1b05a6f6a5e2e06f7ec23b34965537d3fee6
codeots-tds <thedatascape@gmail.com>
2024-10-07 08:57:07 -0400

    Updating sql_queries copy commands with correct syntax

--- 3.data_warehouse_project/sql_queries.py
+++ 3.data_warehouse_project/sql_queries.py
@@ -5,6 +5,7 @@ config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
 # DROP TABLES
+
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
 songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
@@ -15,47 +16,45 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 staging_events_table_create = ("""
-    CREATE TABLE IF NOT EXISTS staging_events (
-    event_id      INT,
-    artist        VARCHAR,
-    auth          VARCHAR,
-    first_name    VARCHAR,
-    gender        CHAR(1),
+CREATE TABLE IF NOT EXISTS staging_events (
+    artist VARCHAR,
+    auth VARCHAR,
+    first_name VARCHAR,
+    gender CHAR(1),
     item_in_session INT,
-    last_name     VARCHAR,
-    length        FLOAT,
-    level         VARCHAR,
-    location      VARCHAR,
-    method        VARCHAR,
-    page          VARCHAR,
-    registration  BIGINT, 
-    session_id    INT, 
-    song          VARCHAR,
-    status        INT, 
-    ts            BIGINT,
-    user_agent    VARCHAR,
-    user_id       INT
+    last_name VARCHAR,
+    length FLOAT,
+    level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration BIGINT,
+    session_id INT,
+    song VARCHAR,
+    status INT,
+    ts BIGINT,
+    user_agent VARCHAR,
+    user_id INT
 );
 """)
 
 staging_songs_table_create = ("""
-CREATE TABLE IF NOT EXISTS staging_songs (
-    num_songs        INT,
-    artist_id        VARCHAR,
-    artist_latitude  FLOAT,
+CREATE TABLE IF NOT EXISTS staging_songs(
+    num_songs INT,
+    artist_id VARCHAR,
+    artist_latitude FLOAT,
     artist_longitude FLOAT,
-    artist_location  VARCHAR,
-    artist_name      VARCHAR,
-    song_id          VARCHAR,
-    title            VARCHAR,
-    duration         FLOAT,
-    year             INT 
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    song_id VARCHAR,
+    title VARCHAR,
+    duration FLOAT,
+    year INT
 );
 """)
 
-# Fact Table
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays (
+CREATE TABLE IF NOT EXISTS songplays(
     songplay_id INT IDENTITY(0,1) NOT NULL PRIMARY KEY,
     start_time TIMESTAMP NOT NULL,
     user_id INT NOT NULL,
@@ -69,7 +68,7 @@ CREATE TABLE IF NOT EXISTS songplays (
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users(
     user_id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR,
     last_name VARCHAR,
@@ -79,7 +78,7 @@ CREATE TABLE IF NOT EXISTS users (
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS songs (
+CREATE TABLE IF NOT EXISTS songs(
     song_id VARCHAR PRIMARY KEY,
     title VARCHAR NOT NULL,
     artist_id VARCHAR NOT NULL,
@@ -89,7 +88,7 @@ CREATE TABLE IF NOT EXISTS songs (
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists (
+CREATE TABLE IF NOT EXISTS artists(
     artist_id VARCHAR PRIMARY KEY,
     name VARCHAR NOT NULL,
     location VARCHAR,
@@ -99,7 +98,7 @@ CREATE TABLE IF NOT EXISTS artists (
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time (
+CREATE TABLE IF NOT EXISTS time(
     start_time TIMESTAMP PRIMARY KEY,
     hour INT NOT NULL,
     day INT NOT NULL,
@@ -111,58 +110,36 @@ CREATE TABLE IF NOT EXISTS time (
 """)
 
 # STAGING TABLES
-#Copying all data from two distinct datasets into staging tables.
-"""
--Pulling log data from S3 bucket into staging_events table.
--From log_data directory in your S3 bucket
--select the IAM role that allows your Redshift cluster permission to access S3 bucket
--JSON path file tells Redshift how to parse your raw JSON files and map to columns in staging_events table.
--Region is 'us-west-2' where the bucket is located.
-"""
+
+# Staging events copy command
 staging_events_copy = ("""
 COPY staging_events
-FROM 's3://your-bucket/log_data'
-IAM_ROLE 'arn:aws:iam::your-iam-role-id:role/RedshiftCopyRole'
-JSON 's3://your-bucket/log_json_path.json'
+FROM {}
+IAM_ROLE '{}'
+JSON {}
 REGION 'us-west-2';
-""")
+""").format(config.get('S3', 'LOG_DATA'), config.get('IAM_ROLE', 'ARN'), config.get('S3', 'LOG_JSONPATH'))
 
-"""
--Pulling song metadata from S3 into staging_songs table.
--Since song data is structured well, you can use 'auto' keyword so Redshift auto-maps the fields to
-columns in staging_songs table
-"""
+# Staging songs copy command
 staging_songs_copy = ("""
 COPY staging_songs
-FROM 's3://your-bucket/song_data'
-IAM_ROLE 'arn:aws:iam::your-iam-role-id:role/RedshiftCopyRole'
+FROM {}
+IAM_ROLE '{}'
 JSON 'auto'
 REGION 'us-west-2';
-""")
+""").format(config.get('S3', 'SONG_DATA'), config.get('IAM_ROLE', 'ARN'))
+
+print(staging_events_copy)
+print('-----------')
+print(staging_songs_copy)
 
 # FINAL TABLES
 
-# Insert Data
 songplay_table_insert = ("""
-INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-SELECT
-    TIMESTAMP 'epoch' + se.ts/1000 * INTERVAL '1 second' AS start_time,
-    se.user_id,
-    se.level,
-    ss.song_id,
-    ss.artist_id,
-    se.session_id,
-    se.location,
-    se.user_agent
-FROM staging_events se
-JOIN staging_songs ss
-    ON se.song = ss.title
-    AND se.artist = ss.artist_name
-WHERE se.page = 'NextSong';
 """)
 
 user_table_insert = ("""
-INSERT INTO users (user_id, first_name, last_name, gender, level)
+INSERT INTO users(user_id, first_name, last_name, gender, level)
 SELECT DISTINCT
     se.user_id,
     se.first_name,
@@ -174,7 +151,7 @@ WHERE se.user_id IS NOT NULL;
 """)
 
 song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration)
+INSERT INTO songs(song_id, title, artist_id, year, duration)
 SELECT DISTINCT
     ss.song_id,
     ss.title,
@@ -186,13 +163,13 @@ WHERE ss.song_id IS NOT NULL;
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude)
+INSERT INTO artists(artist_id, name, location, latitude, longitude)
 SELECT DISTINCT
     ss.artist_id,
-    ss.artist_name AS name,
-    ss.artist_location AS location,
-    ss.artist_latitude AS latitude,
-    ss.artist_longitude AS longitude
+    ss.name,
+    ss.location as location,
+    ss.latitude as latitude,
+    ss.longitude as longitude,
 FROM staging_songs ss
 WHERE ss.artist_id IS NOT NULL;
 """)
@@ -212,6 +189,7 @@ WHERE se.ts IS NOT NULL;
 """)
 
 # QUERY LISTS
+
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
